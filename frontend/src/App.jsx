31244dd13e2835ec8eda375729aaee59be52a89a
shalzzA <shalinijagan195@gmail.com>
2025-07-19 15:58:59 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/dashboard";
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/login" />;
+};
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
+}
+
+export default App;
